cd51527746092fa597c7419200a6754c67b1fcfd
Ragunath Jawahar <rj@mobsandgeeks.com>
2015-03-07 23:24:16 +0530

    [AnnotationRule] Fix potential NPE.

--- saripaar/src/main/java/com/mobsandgeeks/saripaar/AnnotationRule.java
+++ saripaar/src/main/java/com/mobsandgeeks/saripaar/AnnotationRule.java
@@ -44,7 +44,8 @@ public abstract class AnnotationRule<RULE_ANNOTATION extends Annotation, DATA_TY
      *      this rule is paired.
      */
     protected AnnotationRule(final RULE_ANNOTATION ruleAnnotation) {
-        super(Reflector.getAttributeValue(ruleAnnotation, "sequence", Integer.TYPE));
+        super(ruleAnnotation != null ?
+                Reflector.getAttributeValue(ruleAnnotation, "sequence", Integer.TYPE) : -1);
         if (ruleAnnotation == null) {
             throw new IllegalArgumentException("'ruleAnnotation' cannot be null.");
         }
