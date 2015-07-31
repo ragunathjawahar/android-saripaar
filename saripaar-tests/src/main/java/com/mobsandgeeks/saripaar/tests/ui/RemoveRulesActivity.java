d365fb793edf03012b80be1bbf66558a527a1411
Ragunath Jawahar <rj@mobsandgeeks.com>
2015-07-31 20:31:29 +0530

    [Refactor] Remove unused imports.

--- saripaar-tests/src/main/java/com/mobsandgeeks/saripaar/tests/ui/RemoveRulesActivity.java
+++ saripaar-tests/src/main/java/com/mobsandgeeks/saripaar/tests/ui/RemoveRulesActivity.java
@@ -3,7 +3,6 @@ package com.mobsandgeeks.saripaar.tests.ui;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
